73814500fb44a6b69864f38d388113767b596642
natolotra <natolotra@gmail.com>
2025-09-03 10:44:53 +0200

    feat:ajout du code 3-a-b-c

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ app = FastAPI()
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
+
+
+@app.post("/cars")
+def add_car(identifiant: str, brand: str, model: str):
+    new_car = {"identifiant": identifiant, "brand": brand, "model": model}
+    cars.append(new_car)
+    return {"message": "Car créer", "id": identifiant, status: 201}
+
+@app.get("/cars")
+def get_cars():
+    return cars
+
